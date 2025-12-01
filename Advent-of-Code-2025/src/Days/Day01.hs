e2b862e9981ed111ef41f908af9d2715ece7370a
loleik <lolei+github@lolei.uk>
2025-12-01 10:54:27 +0000

    2025 - Day 1 Part 2 Progress

--- Advent-of-Code-2025/src/Days/Day01.hs
+++ Advent-of-Code-2025/src/Days/Day01.hs
@@ -12,17 +12,31 @@ splitTurns _ = 0
 modularAdd :: Int -> Int -> Int
 modularAdd x y = mod (x + y) 100
 
-part1 :: [String] -> Int
-part1 =
-    length
-    . filter (== 0)
-    . scanl modularAdd 50
-    . map splitTurns
+runTurns :: [String] -> [(Int, Int, Int)]
+runTurns l =
+    scanl
+        (\(cur, _, _) delta -> (modularAdd cur delta, cur, delta))
+        (50, 50, 0)
+        (map splitTurns l)
+
+part1 :: [(Int, Int, Int)] -> Int
+part1 l = length [ total | (total, _, _) <- l, total == 0 ]
+
+part2Cond :: (Int, Int, Int) -> Bool
+part2Cond (_, before, delta) =
+    let raw = before + delta
+    in raw < 0 || raw > 99 && mod raw 100 /= 0
+
+part2 :: [(Int, Int, Int)] -> Int
+part2 = length . filter part2Cond
 
 runDay01 :: IO ()
 runDay01 = do
     input <- readInput "inputs/day01.txt"
-    let output = part1 input
-    putStrLn $ "Part 1: " ++ show output
+    let turns = runTurns input
 
-    putStrLn "Part 2: "
+    let result1 = part1 turns
+    putStrLn $ "Part 1: " ++ show result1
+
+    let result2 = result1 + part2 turns
+    putStrLn $ "Part 2: " ++ show result2
