cf439837c04371d1edc6723319446e0a80693c9e
loleik <lolei+github@lolei.uk>
2025-12-01 10:13:02 +0000

    2025 - Day 1 Part 1

--- Advent-of-Code-2025/src/Days/Day01.hs
+++ Advent-of-Code-2025/src/Days/Day01.hs
@@ -1,11 +1,28 @@
 module Days.Day01 (runDay01) where
 
 import Utils.Parse ( readInput )
-import qualified Data.Set as S
+
+splitTurns :: String -> Int
+splitTurns (d:n)
+    | d == 'R'  = read n
+    | d == 'L'  = - read n
+    | otherwise = 0
+splitTurns _ = 0
+
+modularAdd :: Int -> Int -> Int
+modularAdd x y = mod (x + y) 100
+
+part1 :: [String] -> Int
+part1 =
+    length
+    . filter (== 0)
+    . scanl modularAdd 50
+    . map splitTurns
 
 runDay01 :: IO ()
 runDay01 = do
     input <- readInput "inputs/day01.txt"
-    putStrLn "Part 1: "
+    let output = part1 input
+    putStrLn $ "Part 1: " ++ show output
 
     putStrLn "Part 2: "
