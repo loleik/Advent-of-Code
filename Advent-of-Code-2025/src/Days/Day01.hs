75068c87a2f34d834e342aa9fe434803779e27e5
loleik <lolei+github@lolei.uk>
2025-12-01 17:34:16 +0000

    2025 - Day 2

--- Advent-of-Code-2025/src/Days/Day01.hs
+++ Advent-of-Code-2025/src/Days/Day01.hs
@@ -12,23 +12,19 @@ splitTurns _ = 0
 modularAdd :: Int -> Int -> Int
 modularAdd x y = mod (x + y) 100
 
-runTurns :: [String] -> [(Int, Int, Int)]
+runTurns :: [String] -> [Int]
 runTurns l =
-    scanl
-        (\(cur, _, _) delta -> (modularAdd cur delta, cur, delta))
-        (50, 50, 0)
-        (map splitTurns l)
+    scanl modularAdd 50 (map splitTurns l)
 
-part1 :: [(Int, Int, Int)] -> Int
-part1 l = length [ total | (total, _, _) <- l, total == 0 ]
+part1 :: [Int] -> Int
+part1 l = length [ total | total <- l, total == 0 ]
 
-part2Cond :: (Int, Int, Int) -> Bool
-part2Cond (_, before, delta) =
-    let raw = before + delta
-    in raw < 0 || raw > 99 && mod raw 100 /= 0
-
-part2 :: [(Int, Int, Int)] -> Int
-part2 = length . filter part2Cond
+expand :: String -> [Int]
+expand (d:n)
+    | d == 'R' = replicate (read n) 1
+    | d == 'L' = replicate (read n) (-1)
+    | otherwise = [0]
+expand _ = [0]
 
 runDay01 :: IO ()
 runDay01 = do
@@ -38,5 +34,5 @@ runDay01 = do
     let result1 = part1 turns
     putStrLn $ "Part 1: " ++ show result1
 
-    let result2 = result1 + part2 turns
+    let result2 = part1 (scanl modularAdd 50 (concatMap expand input))
     putStrLn $ "Part 2: " ++ show result2
--- README.md
+++ README.md
@@ -8,6 +8,6 @@ A central repository for my [Advent of Code](https://adventofcode.com) solutions
 | 2021 | 25/50 |  Scala*  |
 | 2023 | 32/50 |   Rust   |
 | 2024 | 35/50 |   Rust   |
-| 2025 |  1/50 | Haskell  |
+| 2025 |  2/50 | Haskell  |
 
 *I did part of 2021 in functional scala, but I no longer have the code. I will probably redo it at some point.
