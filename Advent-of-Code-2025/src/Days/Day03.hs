07d6685d995a0dd58cd00a7a578b1979b56ebebc
loleik <lolei+github@lolei.uk>
2025-12-03 16:35:16 +0000

    2025 - Day 3 Part 2

--- Advent-of-Code-2025/src/Days/Day03.hs
+++ Advent-of-Code-2025/src/Days/Day03.hs
@@ -2,32 +2,35 @@ module Days.Day03 ( runDay03 ) where
 
 import Utils.Parse ( readInput )
 
-maxJolts :: String -> String
-maxJolts s = 
-    case snd (foldr digitScan (Nothing, Nothing) s) of
-        Just (a, b) -> [a, b]
-        Nothing     -> ['0', '0']
+type Acc = (String, Int)
 
-digitScan :: Char 
-             -> (Maybe Char, Maybe (Char, Char)) 
-             -> (Maybe Char, Maybe (Char, Char))
-digitScan d (Nothing, bestPair) = (Just d, bestPair)
-digitScan d (Just bestSuffix, bestPair) =
-    let candidate   = (d, bestSuffix)
-        bestPair'   = case bestPair of
-            Nothing      -> Just candidate
-            Just current -> Just (max current candidate)
-        bestSuffix' = Just (max d bestSuffix)
-    in
-        (bestSuffix', bestPair')
+popLessThan :: Acc -> Char -> Acc
+popLessThan ("", k) _    = ("", k)
+popLessThan (x:xs, k) d
+    | k > 0 && x < d = popLessThan (xs, k - 1) d
+    | otherwise      = (x:xs, k)
 
-part1 :: [String] -> Int
-part1 = sum . map (read . maxJolts)
+step :: Acc -> Char -> Acc
+step (stack, k) d = 
+    let (stack', k') = popLessThan (stack, k) d
+    in (d : stack', k')
+
+monotonicStack :: String -> Int -> String
+monotonicStack s i = 
+    let removals   = length s - i
+        (stack, k) = foldl' step ("", removals) s
+        trimmed    = drop k stack
+    in reverse trimmed
+
+wrapper :: [String] -> Int -> Int
+wrapper l x = sum (map (read . (`monotonicStack` x)) l)
 
 runDay03 :: IO ()
 runDay03 = do
     raw <- readInput "inputs/day03.txt"
 
-    let result1 = part1 raw
+    let result1 = wrapper raw 2
     putStrLn $ "Part 1: " ++ show result1
-    putStrLn "Part 2: "
+
+    let result2 = wrapper raw 12
+    putStrLn $ "Part 2: " ++ show result2
