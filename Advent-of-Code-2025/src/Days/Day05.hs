61d1c1afbeffcd44d5ed2a585c4b0a3b263e58fa
loleik <lolei+github@lolei.uk>
2025-12-05 10:54:11 +0000

    2025 - Day 5 Part 2

--- Advent-of-Code-2025/src/Days/Day05.hs
+++ Advent-of-Code-2025/src/Days/Day05.hs
@@ -1,9 +1,11 @@
 module Days.Day05 ( runDay05 ) where 
 
 import Utils.Parse ( readInput, splitting )
-import Days.Day02 ( matches, expandRange )
+import Days.Day02 ( matches )
 import qualified Data.Text as T
 import Data.Set (fromList, toList)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 parseRange :: [T.Text] -> [(Int, Int)]
 parseRange s =
@@ -26,11 +28,24 @@ part1 :: [Int] -> [(Int, Int)] -> Int
 part1 ids ranges =
     length (filter (`checkRange` ranges) ids)
 
+mergeRanges :: [(Int, Int)] -> [(Int, Int)]
+mergeRanges [] = []
+mergeRanges (r:rs) = go r rs
+    where
+        go current [] = [current]
+        go (lo1, hi1) ((lo2, hi2):rest)
+            | lo2 <= hi1 + 1 = go (lo1, max hi1 hi2) rest
+            | otherwise      = (lo1, hi1) : go (lo2, hi2) rest
+
+internalSize :: (Int, Int) -> Int
+internalSize (lo, hi) = hi - lo + 1
+
 part2 :: [String] -> Int
 part2 ranges = 
-    let xs    = map (splitting '-' . T.pack) ranges
-        final = concatMap expandRange xs
-    in length (dupes final)
+    let xs    = concatMap (parseRange . splitting '-' . T.pack) ranges
+        ms    = mergeRanges (sortBy (comparing fst) xs)
+        sizes = map internalSize ms
+    in sum sizes
 
 runDay05 :: IO ()
 runDay05 = do
